b0537019d94f2a7b581166f3bc6f10c86f330038
pranavamrutkar9 <pranavamrutkar09@gmail.com>
2025-05-10 10:46:28 +0530

    Create printTilln.cpp

---
+++ Fundamentals/Loops/While Loop/printTilln.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter value of n: "<<endl;
+    cin >> n;
+    int i = 0;
+    while (i<=n)
+    {
+        cout<<i<<" ";
+        i += 1;
+    }   
+}
