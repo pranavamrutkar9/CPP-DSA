898a8ce51d71a3cf50e47e054d75d6d219760860
pranavamrutkar9 <pranavamrutkar09@gmail.com>
2025-05-11 13:11:06 +0530

    Create Pattern15.cpp

---
+++ Fundamentals/Patterns/Pattern15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// A B C 
+// D E F
+// G H I
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    char ch = 'A';
+    while(i<=n){
+        int j = 1;
+        while (j<=n)
+        {
+            cout<<ch<<" ";
+            j++;
+            ch++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
