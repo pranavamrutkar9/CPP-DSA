4803e69b97d2df45574a14d872083a17338a2e2e
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-05-16 14:50:31 +0530

    Create 06TotalSetBits.cpp

---
+++ 07 Functions/06TotalSetBits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// nth term = (3*n)+7
+int totalSetBits(int n){
+    int count=0;
+    while(n!=0){
+        if(n&1 == 1) count++;
+        n=n>>1;
+    }
+    return count;
+}
+int main(){
+    int a;
+    cin>>a;
+    cout<<totalSetBits(a);
+}
