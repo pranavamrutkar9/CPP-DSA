2e6d42b659d890cedbda26223dc441cd07fac645
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-05-19 18:48:03 +0530

    Update and rename 05FindUniqueInArray to 05FindUniqueInArray.cpp

--- 08 Arrays/05FindUniqueInArray.cpp
+++ 08 Arrays/05FindUniqueInArray.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int findUnique(int *arr, int size)
+int findUnique(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
     {
         int flag = 0;
-        for (int j = i+1; j < size; j++)
+        for (int j = 0; j < size; j++)
         {
-            if (arr[i]==arr[j])
+            if (i!=j && arr[i]==arr[j])
             {
                 flag = 1;
             }
-            
         }
         if (flag == 0)
         {
             return arr[i];
         }
     }
+    return -1;
 }
 
 int main(){
-    int a[]={2,3,1,6,3,6,2};
+    int a[]={1,3,1,3,6,6,7,0,7};
     int n = (sizeof(a)/sizeof(a[0]));
 
     for (int i = 0; i < n; i++)
     {
-        cout<<a[i];
+        cout<<a[i]<<" ";
     }
     cout<<endl;
     cout<<findUnique(a, n);
-
     
 }
