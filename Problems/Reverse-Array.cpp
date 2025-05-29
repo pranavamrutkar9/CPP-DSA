a6e21033c3744dcbae4230b76feb03db0c7296ae
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-05-29 12:34:33 +0530

    Create Reverse-Array.cpp

---
+++ Problems/Reverse-Array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n){
+    for (int i = 0; i < n/2; i++)
+    {
+        swap(arr[i],arr[n-i-1]);
+    }
+}
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    reverseArray(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<' ';
+    }
+    
+}
