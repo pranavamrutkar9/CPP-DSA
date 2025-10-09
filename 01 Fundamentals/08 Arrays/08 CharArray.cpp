28de127b2729793f1c907bc0845743debe06447f
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-10-09 23:43:25 +0530

    Create 08 CharArray.cpp

---
+++ 01 Fundamentals/08 Arrays/08 CharArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getLength(char name[]){
+    int count = 0;
+    for(int i = 0; name[i]!='\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    char name[20];
+    cin >> name;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Length: "<<getLength(name)<<endl;
+    return 0;
+}
