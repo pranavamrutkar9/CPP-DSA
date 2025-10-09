cd2485d26691acf4855f5eead3f5623313103f48
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-10-09 13:58:24 +0530

    Update 014 CN - SumOfTwoArrays.cpp

--- Problems/014 CN - SumOfTwoArrays.cpp
+++ Problems/014 CN - SumOfTwoArrays.cpp
@@ -1,19 +1,36 @@
 #include <bits/stdc++.h> 
 vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
-	string x = "", y = "";
-	for(int i = 0; i<n; i++){
-		x = x+to_string(a[i]);
-	}
-	for(int i = 0; i<m; i++){
-		y = y+to_string(b[i]);
-	}
-	int p = stoi(x), q = stoi(y);
-	string r = to_string(p+q);
-
+	int i = n-1;
+	int j = m-1;
+	int carry = 0;
 	vector<int> result;
-	for(int i = 0; i<r.length(); i++){
-		int t = r[i] -'0';
-		result.push_back(t);
+	while(i>=0 && j>=0){
+		int sum = a[i] + b[j] + carry;
+		carry = sum/10;
+		sum = sum%10;
+		result.insert(result.begin(), sum);
+		i--;
+		j--;
+	}
+
+	while(i>=0){
+		int sum = a[i]+carry;
+		carry = sum/10;
+		sum = sum%10;
+		result.insert(result.begin(), sum);
+		i--;
+	}
+	while(j>=0){
+		int sum = b[j]+carry;
+		carry = sum/10;
+		sum = sum%10;
+		result.insert(result.begin(), sum);
+		j--;
+	}
+	while(carry!=0){
+		int sum = carry%10;
+		carry = carry/10;
+		result.insert(result.begin(), sum);
 	}
 	return result;
 }
