07f8712ac14f97ac31310e9adc473055a256aef9
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-10-21 23:34:47 +0530

    [1047] [Time Beats: 5.00%] [Memory Beats: 95.34%] - LeetPush

---
+++ Problems/Remove-All-Adjacent-Duplicates-In-String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+            int i =0;
+        while(i<s.length()){
+            if(s[i]==s[i+1]){
+                s.erase(i,2);
+                i=0;
+            }
+            else{
+                i++;
+            }
+        }
+        return s;
+    }
+};
